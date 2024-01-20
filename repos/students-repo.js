d845a8fac365bdf0ed9e0b1c57ca52b2c9d8f06c
JeffBrin <bringolfj@hotmail.com>
2024-01-20 17:10:18 -0500

    Changed users repo to students repo

--- repos/students-repo.js
+++ repos/students-repo.js
@@ -1,24 +1,14 @@
 import Repo from "./repo.js";
 
-<<<<<<< HEAD
 class StudentsRepo extends Repo {
 
     static DB_NAME = "Students"
-=======
-class UsersRepo extends Repo {
-
-    static DB_NAME = "Users"
->>>>>>> 0d5df9fa95f8c9446bc6e373e0c43e32d6a5dc8b
 
     /**
      * Creates the user repo
      */
     constructor() {
-<<<<<<< HEAD
         super(StudentsRepo.DB_NAME);
-=======
-        super(UsersRepo.DB_NAME);
->>>>>>> 0d5df9fa95f8c9446bc6e373e0c43e32d6a5dc8b
     }
 
     /**
@@ -26,7 +16,6 @@ class UsersRepo extends Repo {
      * @param {string} username The username of the user to add
      * @param {string} password The password of the user to add
      */
-<<<<<<< HEAD
     async addStudent(username, password) {
         return await this.collection.insertOne({ _id: username, password: password, classes: [] })
     }
@@ -38,20 +27,6 @@ class UsersRepo extends Repo {
      */
     async getStudent(username) {
         const user = await this.collection.findOne({ _id: username });
-=======
-    async addUser(username, password) {
-        return await this.collection.insertOne({ _id: username, password: password })
-    }
-
-    /**
-     * Authenticates a user.
-     * @param {string} username The username to authenticate
-     * @param {string} password The password of the user
-     * @returns An object containing user data if the user is found, null otherwise.
-     */
-    async getUser(username, password) {
-        const user = await this.collection.findOne({ _id: username, password: password });
->>>>>>> 0d5df9fa95f8c9446bc6e373e0c43e32d6a5dc8b
 
         // No user found
         if (!user) {
@@ -68,5 +43,5 @@ class UsersRepo extends Repo {
 
 }
 
-const USERS_REPO = new UsersRepo()
-export default USERS_REPO
+const studentsRepo = new StudentsRepo()
+export default studentsRepo
