1f06fff6e1d92c470c8f2f855341afa847cb6235
Sebastian6547 <78222535+Sebastian6547@users.noreply.github.com>
2024-01-21 08:58:45 -0500

    studentController

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -12,7 +12,8 @@ import { readFile } from '../helpers/file-manager.js';
 const studentRouter = express.Router();
 
 const maxQuestions = 7;
-let questionCount = 0;
+let questionCount = 3;
+let currentQuestion = 0;
 
 studentRouter.get('/classes', requiresAuth(), (req, res) => {
     let firstName = req.firstName;
@@ -38,7 +39,10 @@ studentRouter.post('/join-class', requiresAuth(), async (req, res) => {
 })
 
 studentRouter.get('/topic', requiresAuth(), async (req, res) => {
-    // TODO: Send these to this endpoint
+    currentQuestion = 0;
+    questionCount = 3;
+    try {
+        // TODO: Send these to this endpoint
     const topicName = req.query.topic
     const classCode = req.query.classCode
 
@@ -70,6 +74,10 @@ studentRouter.get('/topic', requiresAuth(), async (req, res) => {
     "I will ask you to either give me a new question or I will give you a question by the student in the following messages.`)
     const question = await assistant.sendMessageAndGetResponse(req.email, "Ask the first question. Do not respond to me, simply ask the question.")
     res.render('student/topic.hbs', { currentPage: "Topic", username: req.firstName, question: question });
+    } catch (error) {
+        console.log(error)
+    }
+    
 });
 
 studentRouter.post("/submit-answer", requiresAuth(), async (req, res) => {
@@ -79,19 +87,29 @@ studentRouter.post("/submit-answer", requiresAuth(), async (req, res) => {
     const answer = `The following is the student's answer to your previous question: "${msg}". ` +
         'Please respond in the following json format: {"correct": bool, "explanation": ...}. ' +
         "The explanation should follow the following format. 1 sentence saying whether the answer was right or wrong, " +
-        "3 sentences or more explaining the correct solution. Write the explanation directly to the student."
+        "3 sentences or more explaining the correct solution. Write the explanation directly to the student." +
+        "Do not use the word 'sure' in your response."
 
     const response = await assistant.sendMessageAndGetResponse(email, answer);
     res.send(response)
 })
 
 studentRouter.post("/next-question", requiresAuth(), async (req, res) => {
-
+    currentQuestion++;
+    let response = "{}";
+    if (currentQuestion >=Math.min(maxQuestions, questionCount)) {
+        res.send(response);
+        return;
+    }
     const email = req.email;
     //const msg = 'Ask the next question. Do not respond to me, only ask the question. If you have asked all the original questions, respond with "{}".'
     const msg = 'If the student didn\'t answer the question correctly, ask another question similar to the previous one. ' +
-        'If the student answered the question correctly, ask a completely different question about the same topic. '
-    const response = await assistant.sendMessageAndGetResponse(email, msg);
+        'If the student answered the question correctly, ask a completely different question about the same topic.' +
+        'Please respond in the following json format: {"correct": bool, "explanation": ...}. '
+    response = await assistant.sendMessageAndGetResponse(email, msg);
+    if (response.correct === "false") {
+        questionCount++;
+    }
     res.send(response);
 })
 
